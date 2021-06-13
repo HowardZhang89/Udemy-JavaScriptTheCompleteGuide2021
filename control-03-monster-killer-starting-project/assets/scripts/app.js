3a706f740995bb264e468c11e0d2843e98181e4d
Howard Zhang <howard.zhang89@gmail.com>
2021-06-13 16:55:37 -0400

    Bonus Life functionality added.

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -6,10 +6,21 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound(){
+  let initialPlayerHealth = currentPlayerHealth;
+
+  if(currentPlayerHealth <=0 && hasBonusLife){
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);   // updates it in the HTML
+    alert('Bonus Life Used!');
+  }
+  
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){

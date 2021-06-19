ae80de6ae6b7fdbf078cd6a5c354d0a0a8b3dabe
Howard Zhang <howard.zhang89@gmail.com>
2021-06-18 20:01:37 -0400

    Added for of / for in loop to print individual log entries.

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt("Maximum HP for You and Monster.", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
@@ -149,6 +150,22 @@ function healPlayerHandler() {
 
 function printLogHandler() {
   console.log(battleLog);
+  for(let i = 0; i < 3; i++){
+    console.log('-------------------');
+  }
+  let i = 0;
+  for(const logEntry of battleLog){
+    if(!lastLoggedEntry && lastLoggedEntry !== 0 || lastLoggedEntry < i){
+      console.log(`#${i}`);
+      for(const key in logEntry){
+        console.log(`${key}: ${logEntry[key]}`);
+    }
+    lastLoggedEntry = i;
+    break;
+    }
+    i++;
+    
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
